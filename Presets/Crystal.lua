e2ebff0b94ad2207605f47121ec4ee5ae3a29131
lcsezX <linconlcarter@gmail.com>
2026-06-04 17:02:43 -0400

    Create Crystal.lua

---
+++ Presets/Crystal.lua
@@ -0,0 +1,8 @@
+return {
+    Name = "Crystal",
+    Primary = Color3.fromRGB(100, 200, 255),
+    Secondary = Color3.fromRGB(30, 40, 65),
+    Background = Color3.fromRGB(20, 25, 45),
+    Text = Color3.fromRGB(255, 255, 255),
+    Border = Color3.fromRGB(150, 220, 255)
+}
