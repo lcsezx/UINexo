4c5e927a47aadb976abbee72a1512e9f74048158
lcsezX <linconlcarter@gmail.com>
2026-06-04 17:01:59 -0400

    Create Nebula.lua

---
+++ Presets/Nebula.lua
@@ -0,0 +1,8 @@
+return {
+    Name = "Nebula",
+    Primary = Color3.fromRGB(189, 0, 255),
+    Secondary = Color3.fromRGB(25, 15, 40),
+    Background = Color3.fromRGB(12, 8, 25),
+    Text = Color3.fromRGB(255, 255, 255),
+    Border = Color3.fromRGB(189, 0, 255)
+}
